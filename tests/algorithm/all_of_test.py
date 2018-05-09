f50a54dc26f0cd05c8e292ab69bbb16c10c4f307
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2018-05-09 14:46:42 +0200

    add passing test: all_of 5 even numbers are even

--- tests/algorithm/all_of_test.py
+++ tests/algorithm/all_of_test.py
@@ -1,5 +1,7 @@
 import std
 
+EVEN_NUMBERS = (0, 2, -1010, -64, 12)
+
 
 def is_even(n):
     return n % 2 == 0
@@ -7,3 +9,7 @@ def is_even(n):
 
 def test_all_of_empty_are_even():
     assert std.all_of((), is_even)
+
+
+def test_all_of_even_numbers_are_even():
+    assert std.all_of(EVEN_NUMBERS, is_even)
