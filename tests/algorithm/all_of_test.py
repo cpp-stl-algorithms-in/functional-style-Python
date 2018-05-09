15b187c045949821f487f378b282931abe3265ed
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2018-05-09 18:14:20 +0200

    test for actual boolean value instead of truthy/falsy

--- tests/algorithm/all_of_test.py
+++ tests/algorithm/all_of_test.py
@@ -8,13 +8,13 @@ def is_even(n):
     return n % 2 == 0
 
 
-def test_all_of_empty_are_even():
-    assert std.all_of((), is_even)
+def test_all_of_with_empty_set_returns_true():
+    assert std.all_of((), is_even) is True
 
 
-def test_all_of_even_numbers_are_even():
-    assert std.all_of(EVEN_NUMBERS, is_even)
+def test_all_of_with_all_elements_fulfilling_predicate_returns_true():
+    assert std.all_of(EVEN_NUMBERS, is_even) is True
 
 
-def test_not_all_of_even_numbers_are_even():
-    assert not std.all_of(MIXED_NUMBERS, is_even)
+def test_all_of_with_some_elements_not_fulfilling_predicate_returns_false():
+    assert std.all_of(MIXED_NUMBERS, is_even) is False
